2acd15ea0b0d7267b4d6ca0d11b0919ef7b50830
MuneneCalvin <munenecalvn@gmail.com>
2023-05-30 09:39:30 +0300

    Fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import iconArrow from "../assets/images/icon-arrow.svg";
 import { AppContext } from "../App";
 
