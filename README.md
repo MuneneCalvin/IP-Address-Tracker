ba01fa25b42145b4ac5e2637581ff4d893f66e56
MuneneCalvin <munenecalvn@gmail.com>
2023-06-03 20:48:19 +0300

    IP Address

---
+++ README.md
@@ -0,0 +1,42 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+  - [The challenge](#the-challenge)  
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [Author](#author)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Sass/scss
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+
+## Author
+
+- Frontend Mentor - [https://www.frontendmentor.io/profile/OneManBanned](https://www.frontendmentor.io/profile/OneManBanned)
+- Twitter - [@https://twitter.com/LawlessWebDev](https://twitter.com/LawlessWebDev)
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,178 @@
-import { useState, createContext } from "react";
-import "./App.css";
-import Map from "./components/Map";
-import SearchBar from "./components/SearchBar";
-import Result from "./components/Result";
-
-export const AppContext = createContext();
+import { useState, useEffect, useRef } from 'react'
+import { useMap } from 'react-leaflet/hooks'
+import markerIcon from './assets/icon-location.svg'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import L from 'leaflet'
 
 function App() {
-  const [lng, setLng] = useState(3.3792);
-  const [lat, setLat] = useState(6.5244);
-  const [userIp, setUserIp] = useState("");
-  const [location, setLocation] = useState("");
-  const [timeZone, setTimeZone] = useState("");
-  const [isp, setISP] = useState("");
+
+  const [userInput, setUserInput] = useState('')
+  const [userCoordinates, setUserCoordinates] = useState([52.4275, -1.84287])
+  const [userData, setUserData] = useState({
+    IP: '192.212.174.101',
+    city: 'Brooklyn',
+    region: 'NY',
+    postalCode: '10001',
+    timezone: '-05:00',
+    isp: 'SpaceX Starlink'
+  })
+  const [placeholderText, setPlaceholderText] = useState('')
+  const [errMsg, setErrMsg] = useState('')
+
+  const input = useRef(null)
+  const btn = useRef(null)
+
+  let address = `${userData.city}, ${userData.region} ${userData.postalCode}`
+  let inputParam = 'ipAddress'
+
+  window.addEventListener('load', placeholderTextFunc)
+  window.addEventListener('resize', placeholderTextFunc)
+
+  useEffect(() => {
+    fetchData(true)
+  }, [])
+
+  function customIcon() {
+    return L.icon({
+      iconUrl: `${markerIcon}`,
+      iconSize: [46, 56]
+    })
+  }
+
+  function fetchData(firstLoad) {
+    let url = ''
+    if (firstLoad) {
+      url = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_IhQt976oG5TLZY7NOFQDdbiA5m6rh&'
+    } else {
+      url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_IhQt976oG5TLZY7NOFQDdbiA5m6rh&${inputParam}=${userInput}`
+    }
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error(res.status);
+      })
+      .then(data => {
+        setUserCoordinates([data.location.lat, data.location.lng])
+        setUserData({
+          IP: data.ip,
+          city: data.location.city,
+          region: data.location.region,
+          postalCode: data.location.postalCode,
+          timezone: data.location.timezone,
+          isp: data.isp
+        })
+      })
+      .catch(() => {
+        let param = inputParam === 'domain' ? inputParam : 'IP address'
+        setErrMsg(`${param} not found`)
+        setUserInput('')
+        setPlaceholderText('')
+      })
+  }
+
+  function placeholderTextFunc() {
+    if (window.innerWidth < 500) {
+      setPlaceholderText('')
+      setErrMsg('')
+    } else {
+      setPlaceholderText('Search for any IP address or domain')
+      setErrMsg('')
+    }
+  }
+
+  function onChange(e) {
+    setErrMsg('')
+    setUserInput(e.target.value)
+  }
+
+  function submitIP(e) {
+    e.preventDefault()
+    if (!e.target.checkValidity()) {
+      if (e.target[0].validity.valueMissing) {
+        if (window.innerWidth < 500) {
+          setPlaceholderText('')
+          setErrMsg('required')
+        } else {
+          setPlaceholderText('')
+          setErrMsg('Enter an IP address or domain name')
+        }
+      }
+      return
+    }
+    ipOrDom(userInput)
+      ? inputParam = 'ipAddress'
+      : inputParam = 'domain'
+    fetchData(false)
+  }
+
+  // ip address returns true : domain name returns false
+  function ipOrDom(str) {
+    const regex = new RegExp('[0-9]+')
+    let input = str.split('.').join().replaceAll(',', '')
+    return regex.test(input)
+  }
+
+  function onFocus() {
+    setErrMsg('')
+  }
+
+  function MyComponent() {
+    const map = useMap()
+    map.panTo(new L.LatLng(userCoordinates[0], userCoordinates[1]))
+    map.setZoom(14)
+    return null
+  }
 
   return (
-    <div>
-      <AppContext.Provider
-        value={{
-          lng,
-          setLng,
-          lat,
-          setLat,
-          userIp,
-          setUserIp,
-          location,
-          setLocation,
-          timeZone,
-          setTimeZone,
-          isp,
-          setISP,
-        }}
+    <main>
+      <section>
+        <h1>IP Address Tracker</h1>
+        <form noValidate onSubmit={(e) => submitIP(e)}>
+          <span
+            onClick={() => input.current.focus()}>{errMsg}</span>
+          <label htmlFor="input">IP addres</label>
+          <input
+            type="text"
+            value={userInput}
+            placeholder={placeholderText}
+            onChange={(e) => onChange(e)}
+            onFocus={onFocus}
+            id='input'
+            required
+            ref={input}
+            className={errMsg ? 'err' : null}
+            autoFocus />
+          <button
+            onClick={() => btn.current.focus()}
+            ref={btn}
+            aria-label='Submit'></button>
+        </form>
+        <dl className='outputContainer'>
+          <div><dt>IP ADDRESS</dt><dd>{userData.IP}</dd></div>
+          <div><dt>LOCATION</dt><dd>{address}</dd></div>
+          <div><dt>TIMEZONE</dt><dd>{`UTC ${userData.timezone}`}</dd></div>
+          <div><dt>ISP</dt><dd>{userData.isp}</dd></div>
+        </dl>
+      </section>
+      <MapContainer center={[userCoordinates[0], userCoordinates[1]]}
+        scrollWheelZoom={true}
+        zoomControl={false}
       >
-        <div className="search-area">
-          <SearchBar />
-          <Result />
-        </div>
-        <Map />
-      </AppContext.Provider>
-    </div>
-  );
+        <MyComponent />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={[userCoordinates[0], userCoordinates[1]]}
+          icon={customIcon()}
+          keyboard={false}
+        >
+        </Marker>
+      </MapContainer>
+    </main>
+  )
 }
 
-export default App;
+export default App
